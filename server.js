c127b4ed96586ba57c553d9cc08b329d7d258c1a
Rita <rita@dyn-129-236-238-161.dyn.columbia.edu>
2019-05-01 22:24:42 -0400

    login

--- server.js
+++ server.js
@@ -43,55 +43,14 @@ app.use(session({
 app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); 
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'hbs'); 
-// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-});
 
-var hbsContent = {userName: '', loggedin: false, title: "You are not logged in today", body: "Hello World"}; 
-
-
-
-
-
-
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-
-
-
-
-// Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-
-
-// Routes
-// =============================================================
-// require("./routes/post-api-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/auth-routes.js")(app);
-
-// require("./routes/api-routes.js")(app);
-
-// Rita
-// app.use(require("./routes/api-routes"));
-// app.use(require("./routes/html-routes"));
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
   });
 // });
